4875ff38c4d9bcb7bfcdbf798b36b5bf725238d7
meetkome <kvictory1010@gmail.com>
2023-01-12 20:32:12 +0100

    commit change

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -7,6 +7,7 @@ document.getElementById("year").innerHTML = year;
 let text = document.lastModified;
 document.getElementById("lastModified").innerHTML = text;
 
+// HArmburger/Navigation Menu
 const menubutton = document.querySelector("#menu-button");
 const menuitems = document.querySelectorAll(".menu-item");
 // menuitems will be a Node List of the list items
@@ -15,3 +16,10 @@ menubutton.addEventListener("click", () => {
 	menuitems.forEach((item) => item.classList.toggle("open"));
 	menubutton.classList.toggle("close");
 });
+// dark mode 
+const modeButton = document.querySelector("#mode");
+const mainArea = document.querySelector("main");
+modeButton.addEventListener("click", () => {
+	modeButton.classList.toggle("dark");
+	mainArea.classList.toggle("dark");
+})
