1d1f2c8308bdf120df00f13e002539f4350b408a
meetkome <kvictory1010@gmail.com>
2023-01-12 12:40:16 +0100

    commit change

---
+++ scripts/week02/bom.js
@@ -0,0 +1,14 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+
+button.addEventListener('click', () => )
+
+// check to make sure the input is not blank before doing the following remaining tasks in this list using an if block, otherwise provide a message or at least do nothing and return the .focus() to the input field.
+if (input.value != '') { ... }
+
+// create an li element
+const li = document.createElement('li');
+
+const deleteButton = document.createElement('button');
+
