7e705b74288287b02f07bc0f75d642095cf80c59
meetkome <kvictory1010@gmail.com>
2023-01-20 00:14:10 +0100

    add new files

--- week03/lazyload.js
+++
@@ -1,7 +0,0 @@
-//get current year
-let currentYear = new Date().getFullYear();
-document.getElementById("year").textContent = currentYear;
-
-//get last modified date
-let lastModified = document.lastModified;
-document.getElementById("lastModified").textContent = lastModified;
