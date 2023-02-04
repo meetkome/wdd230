94e3ab802f5179db9d606bf571115122572eaba4
meetkome <kvictory1010@gmail.com>
2023-02-05 00:22:28 +0100

    new commit

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -27,3 +27,36 @@ numVisits++;
 // store the new number of visits value
 localStorage.setItem("visits-ls", numVisits);
 
+// WEATHER
+const weatherIcon = document.querySelector('#weatherIcon');
+const captionDesc = document.querySelector('#weatherDesc');
+const currentTemp = document.querySelector('#weatherTemp');
+
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Lagos,566&appid=82eed2cbba4b15bb351f841f1e08d31a&units=imperial'; 
+
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}
---
+++ chamber/scripts/directory.js
@@ -0,0 +1,73 @@
+const url = "data/members.json";
+
+async function getMembers() {
+    try {
+        const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        // console.log(data.members);
+        displayMembers(data.members);
+      } else {
+        throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  getMembers();
+  
+  function displayMembers(members) {
+    members.forEach((member) => {
+    
+        const memberCards = document.querySelector("#members");
+        
+        const name = document.createElement("h2");
+        name.innerHTML = member.name;
+        
+        const logo = document.createElement("img");
+        logo.setAttribute("src", member.logo);
+        logo.setAttribute("alt", `${member.name} logo`);
+        logo.setAttribute("loading", "lazy");
+        logo.setAttribute("width", "250");
+        logo.setAttribute("height", "250");
+        
+        const url = document.createElement("p");
+        url.innerHTML = `<a href="${member.url}" target="_blank">Website</a>`;
+
+        const address = document.createElement("p");
+        address.innerHTML = member.address;
+
+        const phone = document.createElement("p");
+        phone.innerHTML = member.phone;
+        
+        const level = document.createElement("p");
+        level.innerHTML = member.membershipLevel;
+        
+        const card = document.createElement("section");
+        card.appendChild(logo);
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(url);
+        card.appendChild(level);
+        memberCards.appendChild(card);
+    });
+}
+
+//grid - list 
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("div#members");
+
+gridbutton.addEventListener("click", showGrid);
+listbutton.addEventListener("click", showList);
+
+function showGrid() {
+  display.classList.add("grid");
+	display.classList.remove("list");
+}
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
