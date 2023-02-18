a28e132e7a526f2d2cd49f2cbd0c7998b0a250c8
meetkome <kvictory1010@gmail.com>
2023-02-18 02:15:29 +0100

    new commit

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,15 +1,28 @@
-const today = new Date();
+// const today = new Date();
 
-if (today.getDay() == 0 || today.getDay() >= 4) {
-  document.querySelector("#banner").className = "hide";
+// if (today.getDay() == 0 || today.getDay() >= 4) {
+//   document.querySelector("#banner").className = "hide";
+// } else {
+//   document.querySelector("#banner").className = "show";  
+// }
+
+
+
+const banner = document.querySelector('#banner');
+var DayOfWeek = new Date().getDay();
+
+if (DayOfWeek == 1 || DayOfWeek == 2 || DayOfWeek == 3) {
+    banner.style.display = "block";
+
 } else {
-  document.querySelector("#banner").className = "show";  
+    banner.style.display = "none";
 }
 
 const bannerClose = document.querySelector('#close-banner');
 
 bannerClose.addEventListener('click', function() {
     banner.style.display = "none";
-// document.querySelector(".bannerClose").addEventListener("click", function(){
-//   this.closest("#banner").style.display = "none";
+
 });
+
+
