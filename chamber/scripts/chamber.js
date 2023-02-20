fd7d681d2a73bbb698021648eddfd8402e00da2a
meetkome <kvictory1010@gmail.com>
2023-02-20 02:17:04 +0100

    new commit

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -9,3 +9,18 @@ menulist.onclick = () =>{
   menulist.classList.toggle('bx-x');
   navbar.classList.toggle('open')
 }
+
+// let hamburger = document.querySelector("#hamburger");
+// let closeMenu = document.querySelector("#close-button");
+
+// hamburger.addEventListener("click", function() {
+//     document.querySelector("#menu-items").classList.toggle("mobileMenu");
+//     hamburger.classList.toggle("hide");
+//     closeMenu.classList.toggle("show");
+//     });
+
+// closeMenu.addEventListener("click", function() {
+//     document.querySelector("#menu-items").classList.toggle("mobileMenu");
+//     hamburger.classList.toggle("hide");
+//     closeMenu.classList.toggle("show");
+//     });
